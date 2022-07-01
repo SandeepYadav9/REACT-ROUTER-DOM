93feea56757b3c0e3ffa945ce6df9abe98c7a318
Sandeep Yadav <sandeepuim10@gmail.com>
2022-07-01 11:19:36 +0530

    ROUTERS-FIRST-PAGE-Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h1>ABOUT </h1>
+      <div>
+        <Link to="/"> Logout</Link>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
